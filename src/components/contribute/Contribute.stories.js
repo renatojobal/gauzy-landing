ced8370a7a8ee151371e7b8666eb02324ed3dedc
renatojobal <renatojobal@gmail.com>
2021-06-02 17:59:17 -0500

    init template for each component

---
+++ src/components/contribute/Contribute.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Contribute from "./Contribute";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  title: "Contribute"
+};
+
+export const contributeStorie = () => {
+  return (
+    <Contribute/>
+  );
+};
